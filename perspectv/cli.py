14558f2ad5fa461cfbe286a83b234cf248419563
Jason Stitt <js@jasonstitt.com>
2024-03-17 11:20:50 -0500

    add basic report without persistence

--- perspectv/cli.py
+++ perspectv/cli.py
@@ -13,13 +13,19 @@ from . import search, model, scrape, llm
     default="google/gemini-pro",
     help="Model for summarization (low cost, med strength)",
 )
-def main(domain, dbfile, model_summary):
+@click.option(
+    "--model-report",
+    default="google/gemini-pro",
+    help="Model for report generation (high strength, high cost)",
+)
+def main(domain, dbfile, model_summary, model_report):
     print(f"Summarizing {domain}...")
     engine = create_engine(f"sqlite:///{dbfile}")
     model.Base.metadata.create_all(engine)
     run_discover(engine, domain)
     run_scrape(engine)
     run_extract(engine, model_summary)
+    run_reports(engine, model_report)
 
 
 def run_discover(engine, domain):
@@ -53,7 +59,7 @@ def run_scrape(engine):
             url.final_url = result.final_url
             url.error = result.error
             if not result.error:
-                page = model.Page(url=result.final_url, body=result.body, extract=None)
+                page = model.Page(url=result.final_url, body=result.body)
                 session.add(page)
             session.commit()
 
@@ -65,3 +71,16 @@ def run_extract(engine, model_summary):
             summary = llm.run_prompt("page_extract", model_summary, text=page.body)
             page.extract = summary
             session.commit()
+
+
+def run_reports(engine, model_report):
+    with Session(engine) as session:
+        pages = session.query(model.Page).all()
+        text = "\n------\n".join(page.extract for page in pages)
+    report_names = ["summary"]
+    reports = [
+        llm.run_prompt(f"report_{name}", model_report, text=text)
+        for name in report_names
+    ]
+    for report in reports:
+        print(report)
