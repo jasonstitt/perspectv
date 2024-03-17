8e9e0c2f11ab1112ff0c49c44a703aaba4a21228
Jason Stitt <js@jasonstitt.com>
2024-03-17 09:54:32 -0500

    add basic page content extraction without persistence

--- perspectv/cli.py
+++ perspectv/cli.py
@@ -2,18 +2,25 @@ import click
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
-from . import search, model, scrape
+from . import search, model, scrape, llm
 
 
 @click.command()
 @click.argument("domain")
 @click.option("--dbfile", default="perspectv.db", help="Filename for sqlite database")
-def main(domain, dbfile):
+@click.option(
+    "--model-summary",
+    default="google/gemini-pro",
+    help="Model for summarization (low cost, med strength)",
+)
+def main(domain, dbfile, model_summary):
     print(f"Summarizing {domain}...")
     engine = create_engine(f"sqlite:///{dbfile}")
     model.Base.metadata.create_all(engine)
     with Session(engine) as session:
+        # Fetch URLs we already found
         urls = session.query(model.Url).all()
+        # Discover URLs using search
         if not urls:
             for url in search.fetch_urls(domain):
                 url_obj = model.Url(original_url=url)
@@ -21,12 +28,18 @@ def main(domain, dbfile):
                 urls.append(url_obj)
             session.commit()
         print(f"Found {len(urls)} URLs")
+        # Scrape all the pages that haven't been scraped yet
         by_original = {url.original_url: url for url in urls}
         to_scrape = [url.original_url for url in urls if not url.final_url]
         print(f"Scraping {len(to_scrape)} URLs")
         results = scrape.scrape(to_scrape)
+        # Save the final urls so we have the redirect map
         for result in results:
             url = by_original[result.original_url]
             url.final_url = result.final_url
-            url.body = result.body
             session.commit()
+        # Extract the page contents
+        by_final = {result.final_url: result for result in results if result.final_url}
+        for final_url, result in by_final.items():
+            summary = llm.run_prompt("page_extract", model_summary, text=result.body)
+            print(final_url, summary)
---
+++ perspectv/llm.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import json
+
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+prompt_dir = os.path.join(base_dir, "prompts")
+
+
+def openrouter(model, text):
+    token = os.getenv("OPENROUTER_API_KEY")
+    response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {token}",
+        },
+        data=json.dumps(
+            {
+                "model": model,
+                "max_tokens": 10000,
+                "messages": [{"role": "user", "content": text}],
+            }
+        ),
+    )
+    data = response.json()
+    text = data["choices"][0]["message"]["content"]
+    return text
+
+
+def run_prompt(prompt_name, model, **params):
+    path = os.path.join(prompt_dir, f"{prompt_name}.txt")
+    with open(path, "r") as infile:
+        text = infile.read()
+    text = text.format(**params)
+    return openrouter(model, text)
